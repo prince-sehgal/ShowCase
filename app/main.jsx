edfdbc246ac5242308482b3ca2d980a6363085a6
symbolizee <symbolizee@gmail.com>
2016-11-08 14:44:13 +0530

    Adding sample project into remote repository

---
+++ app/main.jsx
@@ -0,0 +1,55 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+    <div>
+       I am comment box.
+           <h1>{this.props.name}</h1>
+            <CommentList/>
+        </div>
+    );
+  }
+});
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+    <div>
+       I am comment List.  
+             <Comment/>
+        </div>
+       
+    );
+  }
+});
+
+var Comment = React.createClass({
+  render: function() {
+    return (
+    <div>
+       I am comment.   
+            <CommentForm/>
+        </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+    <div>
+       I am comment Form.           
+        </div>
+    );
+  }
+});
+
+
+
+
+
+ReactDOM.render(<CommentBox name="Sample comment"></CommentBox>, app);
+    
+
